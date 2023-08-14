e1cbaad903905528eac0794e3117f393b6087534
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2023-08-14 16:22:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import { getProducts, getProduct } from './services/api.js';
+import { createOrder, retrieveOrder } from './services/klarna.js';
+import express from 'express';
+const app = express();
+import { config } from 'dotenv';
+config();
+ 
+app.get('/', async (req, res) => {
+    const products = await getProducts();
+    console.log(products)
+    const markup = products.map((p) => `<a style="display: block; color: black; border: solid 2px black; margin: 20px; padding: 10px;" href="/products/${p.id}">${p.title} - ${p.price}</a>`).join(' ');
+    res.send(markup);
+})
+ 
+app.get('/products/:id', async (req, res) => {
+    try {
+    const {id} = req.params;
+    const product = await getProduct(id);
+    const klarnaRsponse = await createOrder(product);
+    const markup = klarnaRsponse.html_snippet;
+    // const markup = `<h1>${product.title} - ${product.price} kr </h1> `;
+    res.send(markup);
+    } catch (error) {
+        res.send(error.message);
+    }
+})
+
+app.get('/confirmation', async (req, res) => {
+    const { order_id } = req.query;
+    const getKlarnaAuth = await retrieveOrder(order_id);
+    const {html_snippet} = getKlarnaAuth;
+    // const markup = `<h1>Hello confirmation</h1>`;
+    res.send(html_snippet);
+})
+
+app.listen(process.env.PORT);
+
+
+
---
+++ services/klarna.js
@@ -0,0 +1,87 @@
+import fetch from "node-fetch";
+
+
+export function getKlarnaAuth() {
+    const username = process.env.PUBLIC_KEY;
+    const password = process.env.SECRET_KEY;
+    const auth = `Basic ${Buffer.from(username + ':' + password).toString('base64')}`;
+    return auth;
+}
+
+export async function createOrder(product) {
+    const path = '/checkout/v3/orders';
+    const auth = getKlarnaAuth();
+    // url = https://api.playground.klarna.com/checkout/v3/orders
+    const url = process.env.BASE_URL + path;
+    const method = 'POST';
+    const headers = {
+        'Content-type': 'application/json',
+        authorization: auth 
+    };
+
+    const quantity = 1;
+    const price = product.price * 100; // ???
+    const total_amount = price * quantity;
+    const total_tax_amount = total_amount * 0.2;
+
+    const payload = {
+        purchase_country: 'SE',
+        purchase_currency: 'SEK',
+        locale: 'sv-SE',
+        order_amount: total_amount,
+        order_tax_amount: total_tax_amount,
+        order_lines: [
+            {
+                type: 'physical',
+                reference: product.id,
+                name: product.title,
+                quantity,
+                quantity_unit: 'pcs',
+                unit_price: price,
+                tax_rate: 2500,
+                total_amount,
+                total_discount_amount: 0,
+                total_tax_amount,
+                image_url: product.image
+            }
+        ],
+        merchant_urls: {
+            terms: 'https://www.example.com/terms.html',
+            checkout: 'https://www.example.com/checkout.html',
+            // confirmation: 'https://www.example.com/confirmation.html',
+            // confirmation: `${process.env.CONFIRMATION_URL}?order_id={checkout.order.id}`,
+            confirmation: 'http://localhost:3000/confirmation?order_id={checkout.order.id}',
+            push: 'https://www.example.com/api/push'
+        }
+    };
+    const body = JSON.stringify(payload);
+    const response = await fetch(url, {method, body, headers});
+    const json = await response.json();
+
+    if (response.status === 200 || response.status === 201) {
+        return json;
+    } else {
+        console.error('Error ', json);
+        return {
+            html_snippet: `<h1>${JSON.stringify(json)}</h1>`,
+        }
+    }
+}
+
+export async function retrieveOrder(order_id) {
+    const path = `/checkout/v3/orders/${order_id}`;
+    const auth = getKlarnaAuth();
+    const url = `${process.env.BASE_URL}${path}`;
+    const method = 'GET';
+    const headers = {authorization: auth};
+    const response = await fetch(url, {method, headers});
+
+    if (response.status === 200 || response.status === 201) {
+        const json = await response.json();
+        return json;
+    } else {
+        return {
+            html_snippet: `<h1>${response.status}${response.statusText}</h1>`
+        }
+    }
+}
